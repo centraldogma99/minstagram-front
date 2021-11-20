bfef6e58d637e625dc739e4397e3b64e82dcf2a2
central.dogma99 <central.dogma99@gmail.com>
2021-11-20 17:42:32 +0900

    fixed wrong addr

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,2 +1,2 @@
-export const backServer = "http://http://ec2-3-131-119-122.us-east-2.compute.amazonaws.com:9000"
+export const backServer = "http://ec2-3-131-119-122.us-east-2.compute.amazonaws.com:9000"
 // http://ec2-3-131-119-122.us-east-2.compute.amazonaws.com
