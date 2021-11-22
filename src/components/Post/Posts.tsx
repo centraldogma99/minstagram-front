a143e2837dd4b02686f3bd2a76665fab9bd381e8
central.dogma99 <central.dogma99@gmail.com>
2021-11-22 18:31:08 +0900

    infinite scroll

--- src/components/Post/Posts.tsx
+++ src/components/Post/Posts.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useLayoutEffect, useEffect, useState } from "react";
+import React, { useLayoutEffect, useEffect, useState, useRef, useCallback } from "react";
 import { backServer } from "../../configs/env";
 import { IPost } from "../../types/postTypes"
 import { Snackbar } from "@mui/material";
@@ -8,6 +8,7 @@ import ToastContext from "../../context/ToastContext";
 import { css } from "@emotion/css"
 import FlipCameraAndroidIcon from '@mui/icons-material/FlipCameraAndroid';
 import PostsContext from "../../context/PostsContext";
+import _ from "lodash"
 
 const ReverseButton = css`
   position: fixed;
@@ -15,14 +16,58 @@ const ReverseButton = css`
   right: 50px;
 `
 
+const PAGE_SIZE = 5;
+
 // post를 서버에서 가져오고 저장
 const Posts = (props: { posts?: IPost[], postIds?: string[] }) => {
   const [posts, setPosts] = useState<IPost[]>(props.posts ?? []);
   const [toastOpen, setToastOpen] = useState(false);
   const [toastMessage, setToastMessage] = useState("");
-  const [isFetching, setIsFetching] = useState(true);
   const [isReverse, setIsReverse] = useState(false);
   const [topPost, setTopPost] = useState<number>();
+
+  // 현재 보고 있는 페이지
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [hasNext, setHasNext] = useState<boolean>(true);
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    if (!props.posts && hasNext) {
+      console.log('go!')
+      // if(currentPage )
+      const f = async () => {
+        try {
+          const res: any = await axios.get(`${backServer}/posts/`, { params: { pageSize: PAGE_SIZE, page: currentPage } })
+          console.log(res)
+          setPosts(prev => [...prev, ...(res.data.data as IPost[])])
+        } catch (e: any) {
+          if (e.response.status === 400) setHasNext(false);
+          else console.log(e);
+        }
+      }
+      f();
+    }
+  }, [currentPage, hasNext])
+
+  // The scroll listener
+  const handleScroll = useCallback((e) => {
+    const element = e.target;
+    const isScrolledToBottom = element.scrollHeight - element.scrollTop === element.clientHeight;
+    if (isScrolledToBottom) setCurrentPage(prev => prev + 1)
+  }, []);
+
+  // Attach the scroll listener to the div
+  useEffect(() => {
+    const throttled = _.throttle(handleScroll, 300)
+    if (ref.current) {
+      ref.current.addEventListener('scroll', throttled)
+    }
+    return () => {
+      if (ref.current) {
+        ref.current.removeEventListener('scroll', throttled)
+      }
+    }
+  }, [handleScroll, ref.current]);
 
   const renderPosts = () => {
     let ps = posts;
@@ -38,21 +83,6 @@ const Posts = (props: { posts?: IPost[], postIds?: string[] }) => {
     return ps.map((post, i) => <Post key={post._id} post={post} order={isReverse ? ps.length - i - 1 : i} />)
   }
 
-  // 이 컴포넌트가 posts prop 없이 호출될 경우
-  // 백엔드에서 포스트 요청하기
-  useLayoutEffect(() => {
-    if (posts.length === 0) {
-      axios.get(`${backServer}/posts/`, { params: { pageSize: 50 } })
-        .then(res => {
-          setPosts(res.data as IPost[]);
-          setIsFetching(false);
-        })
-        .catch(e => console.log(e))
-    } else {
-      setIsFetching(false);
-    }
-  }, [posts])
-
   const handleToastClose = (
     event: React.SyntheticEvent | React.MouseEvent,
     reason?: string,
@@ -65,13 +95,12 @@ const Posts = (props: { posts?: IPost[], postIds?: string[] }) => {
   };
 
   return (
-    <div className="posts">
+    <div className="posts" ref={ref}>
       <ToastContext.Provider value={{ setToastOpen, toastMessage, setToastMessage }}>
         <PostsContext.Provider value={{
           deletePost: (i: number) => {
             setPosts(prev => [...prev.slice(0, i), ...prev.slice(i + 1)])
           },
-          // 불변성을 훼손하지 않도록.
           editPost: (i: number, text: string) => {
             setPosts(prev =>
               [...prev.slice(0, i),
@@ -80,20 +109,12 @@ const Posts = (props: { posts?: IPost[], postIds?: string[] }) => {
             setTopPost(i)
           }
         }} >
-          {!isFetching && !posts &&
+          {!posts &&
             <div>
               표시할 포스트가 없습니다.
             </div>
           }
-          {/* {posts && posts.map((post, i) => {
-            const { _id } = post;
-            return <Post key={_id} post={post} order={i} />
-          })} */}
           {posts && renderPosts()}
-          {/* {posts && posts.reverse().map((post, i) => {
-          const { _id } = post;
-          return <Post key={_id} {...post} setPost={setPost(i)} postComeFirst={() => postComeFirst(i)} />
-        })} */}
         </PostsContext.Provider>
       </ToastContext.Provider>
       <FlipCameraAndroidIcon
